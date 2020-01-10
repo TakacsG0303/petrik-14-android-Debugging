de14f7754a7a578d1517a292ead54bc3242afaca
palflaszlo <pallaszlo15@gmail.com>
2020-01-10 14:43:13 +0100

    hibák javítva

--- app/src/main/java/com/example/debugging/MainActivity.java
+++ app/src/main/java/com/example/debugging/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
 
-
                 }
                 else
                 {
